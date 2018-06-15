2af5a57d8084c3f1a8bf53a92afce2fa89c6ead4
shuvikash <shuvikash.skp@gmail.com>
2018-06-15 18:52:46 +0530

    removed comment of CORS origin for accessing data through react

--- src/main/java/hello/CourseController.java
+++ src/main/java/hello/CourseController.java
@@ -11,14 +11,14 @@ public class CourseController {
     CourseRepository repository;
 
     @RequestMapping(method=RequestMethod.GET , path="/courses")
-    //@CrossOrigin(origins = "http://localhost:8080")
+    @CrossOrigin(origins = "http://localhost:8080")
     public Iterable getCourse()
     {
         return repository.findAll();
 
     }
     @RequestMapping(method=RequestMethod.POST , path="/courses")
-    //@CrossOrigin(origins = "http://localhost:8080")
+    @CrossOrigin(origins = "http://localhost:8080")
     public String postCourse(@RequestBody Course course){
         repository.save(course);
         return "ok";
